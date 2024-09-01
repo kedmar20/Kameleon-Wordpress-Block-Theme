719138d1183d38c112f7c680d5ca77142031cd28
MK <m.kedzierski76@gmail.com>
2024-09-01 07:12:39 -0700

    add files style.css, script1.js, meta tags in functions.php

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+
+wp_enqueue_style( 'style', get_stylesheet_uri() );
+
+wp_enqueue_script( 'script1', get_template_directory_uri() . '/js/script1.js', array( 'jquery' ), 1.1, true);
+
+add_action('wp_head', 'your_function_name');
+function your_function_name(){
+?>
+<meta name="author" content="Martin Kedzierski" />
+<?php
+};
+
+
